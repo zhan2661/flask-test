e48323517f206b7fe3e4da84a6afd09b051914dd
zhan2661 <zhan2661@umn.edu>
2018-03-13 08:52:01 -0500

    flask

--- app.py
+++ app.py
@@ -18,12 +18,12 @@ db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
-class Post(db.Model):
+class Post(db.Model):##create the Postform
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     content = db.Column(db.String(10000))
     
-class User(UserMixin, db.Model):
+class User(UserMixin, db.Model):##create the userform
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(15),unique=True)
     email = db.Column(db.String(50),unique=True)
@@ -32,12 +32,12 @@ class User(UserMixin, db.Model):
 def load_user(user_id):
     return User.query.get(int(user_id))
     
-class LoginForm(FlaskForm):
+class LoginForm(FlaskForm):##login form
     username = StringField('username', validators=[InputRequired(),Length(min=4, max=15)])
     password = PasswordField('password', validators=[InputRequired(),  Length(min=8,max=80)])
     remember = BooleanField('remember me')
 
-class RegisterForm(FlaskForm):
+class RegisterForm(FlaskForm):##redister form
     email =StringField('email', validators =[InputRequired(),Email(message = 'Invalid email'), Length(max=50)])
     username = StringField('username', validators=[InputRequired(),Length(min=4, max=15)])
     password = PasswordField('password', validators=[InputRequired(),  Length(min=8,max=80)])
